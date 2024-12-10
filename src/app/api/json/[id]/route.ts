6588e9253cfac85167c655affdcf3d0e274ae9ec
Rohan Choudhary <rohan.c2002@gmail.com>
2024-12-10 14:40:14 +0530

    bug fix

--- src/app/api/json/[id]/route.ts
+++ src/app/api/json/[id]/route.ts
@@ -1,26 +1,33 @@
 import prisma from "@/lib/db";
 import { NextRequest, NextResponse } from "next/server";
 
-export async function GET(request: NextRequest, { params }: {
+type Params = {
   params: {
     id: string
   }
-}) {
-  const { id } = params;
+}
 
+export async function GET(request: NextRequest, params: Params) {
   try {
     const json = await prisma.jsonData.findUnique({
-      where: { id },
-      select: {
-        id: true,
-        name: true,
-        content: true,
-        createdAt: true
+      where: {
+        id: params.params.id
       }
     });
+
+    if (!json) {
+      return NextResponse.json(
+        { error: 'JSON not found' }, 
+        { status: 404 }
+      );
+    }
+
     return NextResponse.json(json);
-  } catch(error) {
-    console.error('Error fetching JSON data:', error);
-    return NextResponse.json({ error: 'Error fetching JSON data'}, { status: 500 })
+  } catch (error) {
+    console.error('Error fetching JSON:', error);
+    return NextResponse.json(
+      { error: 'Internal Server Error' }, 
+      { status: 500 }
+    );
   }
 }
